ad5e071902d8060038aa3df4e68d6e7349bf3c79
jarek <js100code@gmail.com>
2022-11-30 22:41:57 +0100

    Delete Chooser classes. Pass the font management to the Menu class

--- tkinter/notepad/notepad.py
+++ tkinter/notepad/notepad.py
@@ -19,22 +19,26 @@ class App(tkinter.Tk):
         self.mainloop()
 
     def create_wigets(self):
-        self.menu = Menu(self)
-        self.menu.pack(padx=5, pady=5)
-        self.menu.close.config(command=self.test)
-        self.notes=Notes(self)
+        self.notebook=Notebook(self)
+        self.menu = Menu(self, notebook=self.notebook)
+        self.notebook.config(font=("Arial", 32, "normal"))
+        # print(id(self.notebook))
         # self.notes.config(font=self.menu.fonts_types.chosen_font.get())
-        self.notes.config(font=self.menu.fonts_types.get())
-        
-        self.notes.pack()
-
-        
-    def test(self):
-        print("destrou")
+        # self.notebook.font=(self.menu.fonts_types.change_font,
+        #                  self.menu.sizes_types.chosen_size.get())
+       
+        self.menu.pack(padx=5, pady=5)
+        self.notebook.pack()
+       
 
 class Menu(tkinter.Frame):
-    def __init__(self, master, bg="#bdb9db"):
+    families=['Terminal', 'Modern', 'Lato', 'Roboto', 'Noto', 'Alef', 'Carlito', 'Arial', 'Calibri', 'Impact']
+    sizes=[8,10,12,14,16,20,24,32,48,64,72,96]
+    formats=['normal', 'bold', 'italic']
+
+    def __init__(self, master, notebook):
         super().__init__(master)
+        self.notebook = notebook
         # self.new_img = tkinter.PhotoImage(file='images/new.png')
         # self.new_btn = tkinter.Button(self, image=self.new_img)
         # self.new_btn.grid(row=0, column=0, padx=5, pady=5)
@@ -50,64 +54,51 @@ class Menu(tkinter.Frame):
         # self.close_img = tkinter.PhotoImage(file='images/close.png')
         # self.close_btn = tkinter.Button(self, image=self.close_img)
         # self.close_btn.grid(row=0, column=3, padx=5, pady=5)
+
         self.new=MenuWidgets(self, 'images/new.png')
         self.open=MenuWidgets(self, 'images/open.png', column=1)
         self.save=MenuWidgets(self, 'images/save.png', column=2)
         self.close=MenuWidgets(self, 'images/close.png', column=3)
         self.close.btn.config(command=quit)
-        self.fonts_types=TypeFontChooser(self, column=4)
-        self.sizes_types=SizeFontChooser(self, column=5)
-        self.format_types=FormatFontChooser(self, column=6)
-       
+        
+        self.chosen_font=StringVar()
+        self.chosen_font.set('Robot')
+        self.font_types=tkinter.OptionMenu(self, self.chosen_font, *Menu.families, command=self.change_font)
+        self.font_types.grid(row=0, column=4)
+        
+        self.chosen_size=IntVar()
+        self.chosen_size.set(32)
+        self.size_types=tkinter.OptionMenu(self, self.chosen_size, *Menu.sizes,  command=self.change_font)
+        self.size_types.grid(row=0, column=5)
+
+        self.chosen_format=StringVar()
+        self.chosen_format.set('normal')
+        self.format_types=tkinter.OptionMenu(self, self.chosen_format, *Menu.formats, command=self.change_font)
+        self.format_types.grid(row=0, column=6)
+
+
+    def change_font(self,event):
+        self.notebook.config(font=(self.chosen_font.get(),
+                                   self.chosen_size.get(), 
+                                   self.chosen_format.get()))
 
 class MenuWidgets(tkinter.Button):
+
     def __init__(self, master, image_path, row=0, column=0, padx=5, pady=5):
         super().__init__(master)
         self.img=tkinter.PhotoImage(file=image_path)
         self.btn=tkinter.Button(master, image=self.img) 
         self.btn.grid(row=row,column=column, padx=padx, pady=pady)  
-            
-class TypeFontChooser(tkinter.OptionMenu):
-    families=['Terminal', 'Modern', 'Lato', 'Roboto', 'Noto', 'Alef', 'Carlito', 'Arial', 'Calibri', 'Impact']
-    def __init__(self, master, row=0, column=0, padx=5, pady=5):
-        self.chosen_font=StringVar()
-        self.chosen_font.set('Terminal')
-        self.set = self.chosen_font.set
-        self.get = self.chosen_font.get
-        super().__init__(master, self.chosen_font, *TypeFontChooser.families)
-        self.config(width=16)
-        self.grid(row=row, column=column, padx=padx, pady=pady)
-    
-    def change_font(self, event):
-        def func(event):
-            print('hello') 
-
-        self.config(command=func)
-
-class SizeFontChooser(tkinter.OptionMenu):
-    size=[8,10,12,14,16,20,24,32,48,64,72,96]
-    def __init__(self, master, row=0, column=0, padx=5, pady=5):
-        self.chosen_size=IntVar()
-        self.chosen_size.set(12)
-        super().__init__(master, self.chosen_size, *SizeFontChooser.size)
-        self.config(width=2)
-        self.grid(row=row, column=column, padx=padx, pady=pady)
-
-class FormatFontChooser(tkinter.OptionMenu):
-    formats=['none', 'bold', 'italic']
-    def __init__(self, master, row=0,column=0, padx=5, pady=5):
-        self.chosen_format=StringVar()
-        self.chosen_format.set('none')
-        super().__init__(master, self.chosen_format, *FormatFontChooser.formats)
-        self.config(width=5)
-        self.grid(row=row, column=column, padx=padx, pady=pady)
-
-
-class Notes(tkinter.scrolledtext.ScrolledText):
+      
+class Notebook(tkinter.scrolledtext.ScrolledText):
     text_color = "#fffacd"
     
     def __init__(self, master):
-        super().__init__(master, bg=Notes.text_color, width=400, height=200)
+        super().__init__(master, bg=Notebook.text_color, width=400, height=200)
+
+    
+         
+        
 
         
        
